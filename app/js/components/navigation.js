d16c1674f4452dc813f8d7da97402eb960e21640
alexandraklein <ali.klein@deutschinc.com>
2016-05-09 08:46:21 -0700

    First commit

---
+++ app/js/components/navigation.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    $('a.open-menu').click(function(e){
+        var body = $('body'),
+            menu = $('.menu'),
+            hamburger = $('.hamburger'),
+            isOpen = body.hasClass('open'),
+            scrollValue = isOpen ? -parseInt(body[0].style.top) : body.scrollTop();
+
+        menu.toggleClass('open',!isOpen); // adds 'height: 100%'
+        body.toggleClass('open',!isOpen); // adds 'position: fixed'
+        hamburger.toggleClass('open');
+
+        if(!isOpen){
+            body.css('top', -scrollValue);
+            menu.scrollTop(0);
+        } else {
+            body.scrollTop(scrollValue);
+        }
+    });
+});
---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by alexandraklein on 5/7/16.
+ */
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
