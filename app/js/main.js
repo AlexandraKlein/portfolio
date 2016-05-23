5000d64b19598c8128c6ca94abaef36fadfebc7a
alexandraklein <ali.klein@deutschinc.com>
2016-05-23 09:00:55 -0700

    JS refactor.

--- app/js/main.js
+++ app/js/main.js
@@ -3,29 +3,43 @@
  */
 $( document ).ready(function() {
 
-    console.log( "ready!" );
-
     /**
      * Logo Animation
      */
-    var path = document.querySelector('.logo');
-    var length = path.getTotalLength();
-    path.style.transition = path.style.WebkitTransition = 'none';
-    path.style.strokeDasharray = length + ' ' + length;
-    path.style.strokeDashoffset = length;
-    path.getBoundingClientRect();
-    path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 7s ease-in-out';
-    path.style.strokeDashoffset = '0';
+
+    var $body = $('body');
+
+    if ($body.hasClass('home')) {
+        console.log('Page: Home');
+
+        var path = document.querySelector('.logo');
+        var length = path.getTotalLength();
+        path.style.transition = path.style.WebkitTransition = 'none';
+        path.style.strokeDasharray = length + ' ' + length;
+        path.style.strokeDashoffset = length;
+        path.getBoundingClientRect();
+        path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 7s ease-in-out';
+        path.style.strokeDashoffset = '0';
 
 
-    var hi = new Vivus('machine', {type: 'scenario-sync', duration: 7}, function () {
-        if (console) {
-            console.log('Animation finished. [log triggered from callback]');
-        }
-    });
-    var hello = new Vivus('responsive', {type: 'scenario-sync', duration: 7}, function () {
-        if (console) {
-            console.log('Animation finished. [log triggered from callback]');
-        }
-    });
+    /**
+     * SVG Vivus Animation
+     */
+
+        var hi = new Vivus('machine', {type: 'scenario-sync', duration: 7}, function () {
+            if (console) {
+                console.log('Animation finished. [log triggered from callback]');
+            }
+        });
+        var hello = new Vivus('responsive', {type: 'scenario-sync', duration: 7}, function () {
+            if (console) {
+                console.log('Animation finished. [log triggered from callback]');
+            }
+        });
+    }
+
+    if ($body.hasClass('about')) {
+        console.log('Page: About');
+    }
+
 });
