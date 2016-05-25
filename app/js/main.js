d69688950915c29f95a5c21eab2d9806a22fe791
alexandraklein <ali.klein@deutschinc.com>
2016-05-25 08:46:47 -0700

    About page.

--- app/js/main.js
+++ app/js/main.js
@@ -40,6 +40,15 @@ $( document ).ready(function() {
 
     if ($body.hasClass('about')) {
         console.log('Page: About');
+
+        var path = document.querySelector('.logo');
+        var length = path.getTotalLength();
+        path.style.transition = path.style.WebkitTransition = 'none';
+        path.style.strokeDasharray = length + ' ' + length;
+        path.style.strokeDashoffset = length;
+        path.getBoundingClientRect();
+        path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 7s ease-in-out';
+        path.style.strokeDashoffset = '0';
     }
 
 });
