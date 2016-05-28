bfb447de2c4cd25ea66e474d92b8c0b6343c211a
alexandraklein <ali.klein@deutschinc.com>
2016-05-28 10:17:29 -0700

    Projects layout.

--- app/js/main.js
+++ app/js/main.js
@@ -7,25 +7,26 @@ $( document ).ready(function() {
      * Logo Animation
      */
 
+    var path = document.querySelector('.logo');
+    var length = path.getTotalLength();
+    path.style.transition = path.style.WebkitTransition = 'none';
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    path.getBoundingClientRect();
+    path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 7s ease-in-out';
+    path.style.strokeDashoffset = '0';
+
     var $body = $('body');
 
+    /**
+     * Body Scripts
+     */
     if ($body.hasClass('home')) {
         console.log('Page: Home');
-
-        var path = document.querySelector('.logo');
-        var length = path.getTotalLength();
-        path.style.transition = path.style.WebkitTransition = 'none';
-        path.style.strokeDasharray = length + ' ' + length;
-        path.style.strokeDashoffset = length;
-        path.getBoundingClientRect();
-        path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 7s ease-in-out';
-        path.style.strokeDashoffset = '0';
-
 
     /**
      * SVG Vivus Animation
      */
-
         var hi = new Vivus('machine', {type: 'scenario-sync', duration: 7}, function () {
             if (console) {
                 console.log('Animation finished. [log triggered from callback]');
@@ -38,17 +39,12 @@ $( document ).ready(function() {
         });
     }
 
+    /**
+     * About Scripts
+     */
     if ($body.hasClass('about')) {
         console.log('Page: About');
-
-        var path = document.querySelector('.logo');
-        var length = path.getTotalLength();
-        path.style.transition = path.style.WebkitTransition = 'none';
-        path.style.strokeDasharray = length + ' ' + length;
-        path.style.strokeDashoffset = length;
-        path.getBoundingClientRect();
-        path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 7s ease-in-out';
-        path.style.strokeDashoffset = '0';
     }
+
 
 });
