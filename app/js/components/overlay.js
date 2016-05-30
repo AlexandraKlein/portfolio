6fb09d83a9a46777ca4ad555202247d8c5682efd
alexandraklein <ali.klein@deutschinc.com>
2016-05-30 16:45:08 -0700

    JS: overlay keeps body in same place

--- app/js/components/overlay.js
+++ app/js/components/overlay.js
@@ -3,17 +3,47 @@ $( document ).ready(function() {
     var body = $('body'),
         overlay = $('.project-overlay'),
         close = $('.close'),
-        project = $('a.project-target');
+        project = $('a.project-target'),
+        isOpen = body.hasClass('open'),
+        scrollValue = isOpen ? -parseInt(body[0].style.top) : body.scrollTop();
 
     project.click(function(e){
+        var body = $('body'),
+            overlay = $('.project-overlay'),
+            close = $('.close'),
+            project = $('a.project-target'),
+            isOpen = body.hasClass('open'),
+            scrollValue = isOpen ? -parseInt(body[0].style.top) : body.scrollTop();
+
         $(e.currentTarget).siblings('.project-overlay').addClass('open'); // adds 'height: 100%'
         body.addClass('open'); // adds 'position: fixed'
         close.addClass('open');
+
+        if(!isOpen){
+            body.css('top', -scrollValue);
+            overlay.scrollTop(0);
+        } else {
+            body.scrollTop(scrollValue);
+        }
     });
 
     close.click(function (e) {
+
+        var body = $('body'),
+            overlay = $('.project-overlay'),
+            close = $('.close'),
+            isOpen = body.hasClass('open'),
+            scrollValue = isOpen ? -parseInt(body[0].style.top) : body.scrollTop();
+
         overlay.removeClass('open');
         body.removeClass('open');
         close.removeClass('open');
+
+        if(!isOpen){
+            body.css('top', -scrollValue);
+            overlay.scrollTop(0);
+        } else {
+            body.scrollTop(scrollValue);
+        }
     });
 });
