cc588b0e32851a01d84affb5a8308ec337bf23dc
alexandraklein <ali.klein@deutschinc.com>
2016-06-05 12:58:59 -0700

    Fit and finish

--- app/js/main.js
+++ app/js/main.js
@@ -38,4 +38,35 @@ $( document ).ready(function() {
         console.log('Page: About');
     }
 
+    var close = $('.close'),
+        project = $('a.project-target');
+
+    //Open overlay
+    project.click(function(e){
+        var body = $('body'),
+            isOpen = body.hasClass('open'),
+            scrollValue = isOpen ? -parseInt(body[0].style.top) : body.scrollTop();
+
+        $(e.currentTarget).siblings('.project-overlay').addClass('open');
+        body.addClass('open');
+        close.addClass('open');
+
+        body.css('top', -scrollValue);
+    });
+
+    //Close overlay
+    close.click(function () {
+
+        var body = $('body'),
+            overlay = $('.project-overlay'),
+            isOpen = body.hasClass('open'),
+            scrollValue = isOpen ? -parseInt(body[0].style.top) : body.scrollTop();
+
+        overlay.removeClass('open');
+        body.removeClass('open');
+        close.removeClass('open');
+
+        body.scrollTop(scrollValue);
+    });
+
 });
