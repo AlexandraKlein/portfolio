aca297dbf11fa76df237d4ad055f40a465c31dad
alexandraklein <ali.klein@deutschinc.com>
2017-02-11 14:08:43 -0800

    commented out loader

--- app/js/main.js
+++ app/js/main.js
@@ -69,4 +69,69 @@ $( document ).ready(function() {
         body.scrollTop(scrollValue);
     });
 
+    //$(window).load(function(){
+    //    $('#preloader').fadeOut('slow',function(){$(this).remove();});
+    //});
+
+    //preloader
+    //
+    //var demoImgArray = ['images/video/video-1.jpg',
+    //                'images/video/video-2.jpg',
+    //                'images/video/video-3.jpg',
+    //                'images/video/video-4.jpg',
+    //                'images/video/video-5.jpg',
+    //                'images/video/video-6.jpg',
+    //                'images/video/video-7.jpg',
+    //                'images/video/video-8.jpg',
+    //                'images/video/video-9.jpg',
+    //                'images/video/video-10.jpg',
+    //                'images/video/video-11.jpg',
+    //                'images/video/video-12.jpg',
+    //                'images/video/video-13.jpg',
+    //                'images/video/video-14.jpg',
+    //                'images/video/video-15.jpg',
+    //                'images/video/video-16.jpg',
+    //                'images/video/video-17.jpg',
+    //                'images/video/video-18.jpg',
+    //                'images/video/video-19.jpg',
+    //                'images/video/video-20.jpg',
+    //                'images/video/video-21.jpg',
+    //                'images/video/video-22.jpg',
+    //                'images/video/video-23.jpg',
+    //                'images/video/video-24.jpg',
+    //                'images/video/video-25.jpg',
+    //                'images/video/video-26.jpg',
+    //                'images/video/video-27.jpg',
+    //                'images/video/video-28.jpg',
+    //                'images/video/video-29.jpg',
+    //                'images/video/video-30.jpg',
+    //                'images/video/video-31.jpg',
+    //                'images/video/video-32.jpg',
+    //                'images/video/video-33.jpg',
+    //                'images/video/video-34.jpg',
+    //                'images/video/video-35.jpg',
+    //                'images/video/video-36.jpg',
+    //                'images/video/video-37.jpg',
+    //                'images/video/video-38.jpg',
+    //                'images/video/video-39.jpg',
+    //                'images/video/video-40.jpg',
+    //                'images/video/video-41.jpg',
+    //                'images/video/video-42.jpg',
+    //                'images/video/video-43.jpg',
+    //                'images/video/video-44.jpg',
+    //                'images/video/video-45.jpg',
+    //                'images/video/video-46.jpg',
+    //                'images/video/video-47.jpg',
+    //                'images/video/video-48.jpg',
+    //                'images/video/video-49.jpg',
+    //                'images/video/video-50.jpg',
+    //                'images/video/video-51.jpg',
+    //                'images/video/video-52.jpg',
+    //                'images/video/video-53.jpg',
+    //                'images/video/video-54.jpg',
+    //                'images/video/video-55.jpg',
+    //                'images/video/video-56.jpg',
+    //                'images/video/video-57.jpg'];
+
+
 });
